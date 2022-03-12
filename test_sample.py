0ff5d449ea4bc5beb47c79eaeb16fab75260a7f9
kevinhuytran1128 <101434732+kevinhuytran1128@users.noreply.github.com>
2022-03-12 11:54:47 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,7 @@ Test helper functions
 """
 
 import unittest
-from sample import increment_by_two
+from sample import increment_by_two, incremnt_by_three
 
 class TestSampleMethods(unittest.TestCase):
     """
@@ -17,6 +17,14 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_two(-2), 0)
         self.assertEqual(increment_by_two(0), 2)
         self.assertEqual(increment_by_two(3), 5)
+        
+    def increment_by_three(var_to_increment):
+        """
+        This function increments input by three
+        :param var_to_increment: param to increment
+        :return: incremented variable
+        """
+        return var_to_increment + 3
 
 if __name__ == '__main__':
     unittest.main()
